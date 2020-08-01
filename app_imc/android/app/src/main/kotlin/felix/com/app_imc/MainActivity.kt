68a18b06761e2c10839998d1ee04ef801edae400
L. Felix dos Anjos <l.felix.dos.anjos@avanade.com>
2020-08-01 12:37:53 -0300

    first commit

---
+++ app_imc/android/app/src/main/kotlin/felix/com/app_imc/MainActivity.kt
@@ -0,0 +1,6 @@
+package felix.com.app_imc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ conversor/android/app/src/main/kotlin/felix/com/conversor/MainActivity.kt
@@ -0,0 +1,6 @@
+package felix.com.conversor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
